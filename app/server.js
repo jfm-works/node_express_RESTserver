d4002cddeebb165675d8531221d3f214edb0211d
mac-mr9v2j042 <mac-mr9v2j001@mac-mr9v2j042.lan>
2021-10-26 13:24:32 +0900

    login APIのテスト版作成

--- app/server.js
+++ app/server.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const express = require('express');
-
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -9,26 +8,63 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.use(express.static('public'));
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
     res.render('index');
 });
-app.post('/', function(req, res, next) {
-    const response = '{"res":"post"}';
-    res.json(response);
+app.post('/', function(req, res) {
+    const response = req;//'{"res":"post"}';
+    console.log(req);
+    res = response;
 });
-app.put('/', function(req, res, next) {
+app.put('/', function(req, res) {
     const response = '{"res":"put"}';
     res.json(response);
 });
-app.delete('/', function(req, res, next) {
+app.delete('/', function(req, res) {
     const response = '{"res":"delete"}';
-    res.json(response);
+    res.text(response);
 });
 
 
+app.post('/login', function(req, res) {
+    console.log(req.body);
+    console.log(req.body);
+    // console.log(JSON.parse(req.body));
+    const input_id = req.body.id;
+    const input_pw = req.body.pw;
+    console.log(dbdata[input_id],"===",input_pw);
+    if (dbdata[input_id] == "undefined") {
+        console.log("id undefined");
+        return res.send("login failed");
+    }
+    if (dbdata[input_id] === input_pw) {
+        return res.send("login success:"+input_id);
+    }else{
+        return res.send("login failed");
+    }
+    // res.json(req.body);
+    // const inputId = req.body.id;
+    // const inputPw = req.body.pw;
+    // const response = inputId;
+    // console.log(response);
+    // res.send = response;
+});
+
+app.post('/hello', (req, res) => {
+    res.send('Hello ');
+});
+
+app.post('/helloname', (req, res) => {
+    console.log(req.body);
+    res.send('Hello ' + req.body);
+});
+
+//仮DB
+const dbdata={"user001":"password001"}
+
 app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
---
+++ readme.js
@@ -0,0 +1,45 @@
+//
+//
+
+// ログインのため、idとpwをサーバに送信する
+function loginAndGetSessionid(id,pw) {
+    const body = new FormData();
+    body.append('id', id);
+    body.append('pw', pw);
+    return fetch('/login', {
+        method: 'POST',
+        body
+    }).then((response) => showRESTResponse(response));
+}
+
+app.post('/login', function(req, res) {
+    const response = req;//'{"res":"post"}';
+    console.log(req);
+    res = response;
+});
+
+// 認証情報をサーバに送信する
+//<INPUT TYPE="HIDDEN" NAME="MEMBERFLG" VALUE=<%= bolMember %>>
+function checkAccount(params) {
+    
+}
+
+// 着せ替え情報の登録状況を確認する
+
+// 着せ替え情報を着せ替え管理サーバにアップロードする
+
+// アップロード済みの着せ替え情報を削除する
+function uploadedKisekaeDataDelete(){
+
+}
+// アップロード済みの着せ替え情報を変更する
+
+// アップロード済みの着せ替え情報を承認する
+
+// 承認済みの着せ替え情報を変更する
+
+// 承認済みの着せ替え情報を削除する
+
+// 承認済みの着せ替え情報を変更する
+
+// 承認済みの着せ替え情報の公開日になったら、本番サーバにアップロードする
